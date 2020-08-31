2ffc89e5573d29dec51f532f4b71d83073f58b77
杨智康 <55861791+DribbleLife11@users.noreply.github.com>
2020-08-31 23:21:21 +0800

    Z字抖动

---
+++ 字符串相关问题/Zconvert.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int j = 0;
+        int flag = -1;
+        for (char c : s.toCharArray()) {
+            rows.get(j).append(c);
+            if (j == 0 || j == numRows - 1) {
+                flag =  -flag;
+            }
+            j += flag;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder sb : rows) {
+            res.append(sb);
+        }
+        return res.toString();
+    }
+}
