a751106310e783f49d182974f34b42e9e985e294
杨智康 <55861791+DribbleLife11@users.noreply.github.com>
2020-09-06 23:29:43 +0800

    最接近三数之和

---
+++ 数组双指针相关题目/ThreeSumClosed.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int len = nums.length;
+        Arrays.sort(nums);
+        int ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int start = i + 1;
+            int end = len - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - sum) < Math.abs(target - ans)) {
+                    ans = sum;
+                }
+                if (sum > target) {
+                    end--;
+                } else if (sum < target) {
+                    start++;
+                } else {
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
